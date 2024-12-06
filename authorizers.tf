fee934c8cd54b1a1f632a18709331c7f871f7e38
raulsouza-rm355416 <rm355416@fiap.com.br>
2024-12-06 17:39:51 -0300

    fmt

--- authorizers.tf
+++ authorizers.tf
@@ -1,16 +1,16 @@
 resource "aws_api_gateway_authorizer" "auth_clientes" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  name        = "Clientes-ava-Authorizer"
-  type        = "REQUEST"
-  authorizer_uri = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.lambda_auth_clientes_arn}/invocations"
+  rest_api_id     = aws_api_gateway_rest_api.api.id
+  name            = "Clientes-ava-Authorizer"
+  type            = "REQUEST"
+  authorizer_uri  = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.lambda_auth_clientes_arn}/invocations"
   identity_source = "method.request.header.cpf"
 }
 
 resource "aws_api_gateway_authorizer" "auth_funcionarios" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  name        = "Funcionarios-ava-Authorizer"
-  type        = "REQUEST"
-  authorizer_uri = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.lambda_auth_funcionarios_arn}/invocations"
+  rest_api_id     = aws_api_gateway_rest_api.api.id
+  name            = "Funcionarios-ava-Authorizer"
+  type            = "REQUEST"
+  authorizer_uri  = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.lambda_auth_funcionarios_arn}/invocations"
   identity_source = "method.request.header.matricula"
 }
 
